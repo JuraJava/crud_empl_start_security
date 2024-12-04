9175479ade27d56f07e063cf694e040f2d12ac4d
YurDan1973 <jura.danilov.73@gmail.com>
2024-12-04 14:18:28 +0300

    Initial commit

--- src/main/java/com/hstn/crud_empl/security/MySecurityConfiguration.java
+++ src/main/java/com/hstn/crud_empl/security/MySecurityConfiguration.java
@@ -2,9 +2,13 @@ package com.hstn.crud_empl.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 // Эта аннотация говорит о том что этоткласс является конфигурационным
@@ -34,4 +38,23 @@ public class MySecurityConfiguration {
 
         return new InMemoryUserDetailsManager(user1Ivan, user2Oleg, user3Inna);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configer -> configer
+                .requestMatchers(HttpMethod.GET, "/employees").hasRole("EMPLOYEE")
+                // Это означает, что по адресу "/api/employees" (это адрес где используется метод get())
+                // эта страница будет доступна для сотрудника с ролью EMPLOYEE
+                .requestMatchers(HttpMethod.GET, "/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/employees/**").hasRole("ADMIN")
+        );
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        // Здесь используются методы для настройки аутентификации и защиты от CSRF-атак
+
+        return http.build();
+    }
+
 }
